a948eb783716f2a480d038f9a5438db20e4a3499
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2024-05-07 17:28:46 +0530

    double  ll

---
+++ 2816. Double a Number Represented as a Linked List.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode *head){
+        ListNode* prev = NULL;
+        ListNode* curr = head, *next_node;
+
+        while(curr != NULL){
+            next_node = curr -> next;
+            curr -> next = prev;
+
+            prev = curr;
+            curr = next_node;
+        }
+        return prev;
+    }
+
+    ListNode* doubleIt(ListNode* head) {
+        ListNode* rev = reverseList(head);
+        ListNode* temp = rev, *prev = NULL;
+
+        int carry = 0;
+        while(temp != NULL){
+            int value = temp -> val;
+            value *= 2;
+            value += carry;
+
+            if(value > 9){
+                carry = 1;
+            }
+            else{
+                carry = 0;
+            }
+
+            temp -> val = value%10;
+            prev = temp;
+            temp = temp -> next;
+        }
+
+        if(carry != 0){
+            ListNode* extra_node = new ListNode(carry);
+            prev -> next = extra_node;
+        }
+
+        ListNode* ans = reverseList(rev);
+        return ans;
+    }
+};
